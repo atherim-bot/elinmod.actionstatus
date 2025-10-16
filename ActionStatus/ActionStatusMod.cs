b56f2fd706015df8732ebe1e479040c9ac0dd0b6
Andrea <zhang.andrea@outlook.com>
2025-10-16 11:31:47 +0100

    [1.0.1] show combat status

--- ActionStatus/ActionStatusMod.cs
+++ ActionStatus/ActionStatusMod.cs
@@ -8,7 +8,7 @@ public static class ModInfo
 {
     public const string Guid = "atherim.actionstatus";
     public const string Name = "Action Status";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
 }
 
 [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
@@ -17,7 +17,8 @@ internal class ActionStatusMod : BaseUnityPlugin
     private static ActionStatusMod? Instance { get; set; }
 
     private string? _previousAction;
-    private bool _isSleeping = false;
+    private bool _isSleeping;
+    private bool _isInCombat;
 
     private void Awake()
     {
@@ -28,22 +29,38 @@ internal class ActionStatusMod : BaseUnityPlugin
     private void Update()
     {
         if (!Core.Instance.IsGameStarted || EMono.player == null || EMono.player.chara == null) return;
-
-        var playerAction = EMono.player.chara.GetActionText();
+        
+        var playerChara = EMono.player.chara;
+        var playerAction = playerChara.GetActionText();
         
         switch (_isSleeping)
         {
-            case false when EMono.player.chara.conSleep != null:
+            case false when playerChara.conSleep != null:
                 _isSleeping = true;
                 WidgetStatsBarPatch.ActionNotification!.text = "Sleeping";
                 return;
-            case true when EMono.player.chara.conSleep == null:
+            case true when playerChara.conSleep == null:
                 _isSleeping = false;
                 WidgetStatsBarPatch.ActionNotification!.text = "Idle";
                 return;
         }
 
-        if (playerAction == _previousAction) return;
+        switch (_isInCombat)
+        {
+            case false when playerChara.IsInCombat:
+                Log("go to combat");
+                _isInCombat = true;
+                WidgetStatsBarPatch.ActionNotification!.text = "Fighting!";
+                WidgetStatsBarPatch.ActionNotification.Refresh();
+                return;
+            case true when !playerChara.IsInCombat:
+                Log("out of combat");
+                _isInCombat = false;
+                WidgetStatsBarPatch.ActionNotification!.text = "Idle";
+                return;
+        }
+        
+        if (playerAction == _previousAction || _isSleeping || _isInCombat) return;
 
         _previousAction = playerAction;
         
--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Display current action status on the status info widget. E.g. Idle, fishing, col
 ## Known issues
 
 - Doesn't display status properly when walking while holding mouse click. Only works for click go to tile
-- Does not display some statuses like combat
 - Does not distinguish mining from woodcutting from plant gathering
 
 ## Credit
